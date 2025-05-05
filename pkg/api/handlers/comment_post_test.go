93002eda058483ce7f7d0ff33579f1e4d81d2926
EightCubed <sushyam.s@gmail.com>
2025-05-06 01:27:30 +0530

    feat: SOC-25: Removed focussed testing

--- pkg/api/handlers/comment_post_test.go
+++ pkg/api/handlers/comment_post_test.go
@@ -11,7 +11,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = FDescribe("CommentPost", func() {
+var _ = Describe("CommentPost", func() {
 	var (
 		testBody  map[string]interface{}
 		router    *mux.Router
